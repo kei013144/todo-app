3c71e6b6bf44857b801e7b88d480ffb24dd02386
kanaya <t.kanaya0530@gmail.com>
2025-03-12 20:14:58 +0900

    TODOのマークアップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
 import ColorfulMessage from "./components/ColorfulMessage"
 
 function App() {
+  const [num, setNum] = useState(-999);
+
   return (
     <>
-      <h1>TodoApp</h1>
-      <ColorfulMessage message="123456" color="green"/>
+      <div>
+        <input placeholder="TODOを入力"></input>
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODO</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+      <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODO</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
