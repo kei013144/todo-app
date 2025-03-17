5bed9690ea206a6221b8972f3dfe8048bc7a69d9
kanaya <t.kanaya0530@gmail.com>
2025-03-17 19:06:17 +0900

    コード修正

--- src/hooks/useTodoInput.tsx
+++ src/hooks/useTodoInput.tsx
@@ -1,9 +1,10 @@
 import { useState, ChangeEvent } from 'react';
 
 export default function useTodoInput() {
-  const [todoText, setTodoText] = useState("");
-  const onChangeText = (event: ChangeEvent<HTMLInputElement>) => setTodoText(event.target.value);
-  const resetTodoText = () => setTodoText("");
+  const [todoText, setTodoText] = useState('');
+  const onChangeText = (event: ChangeEvent<HTMLInputElement>) =>
+    setTodoText(event.target.value);
+  const resetTodoText = () => setTodoText('');
 
   return { todoText, onChangeText, resetTodoText };
 }
