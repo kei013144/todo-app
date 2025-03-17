4648ea3b210e2e155bd0b3d8d9f8da2108d3cb00
kanaya <t.kanaya0530@gmail.com>
2025-03-17 19:03:57 +0900

    prettier導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import "./App.css"
+import './App.css';
 import useTodoInput from './hooks/useTodoInput';
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
   const [completeTodos, setCompleteTodos] = useState<string[]>([]);
 
   const onClickAdd = () => {
-    if (todoText === "") return;
+    if (todoText === '') return;
 
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
@@ -43,38 +43,44 @@ export default function App() {
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" value={todoText} onChange={onChangeText}></input>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeText}
+        ></input>
         <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
           {incompleteTodos.map((todo, index) => {
-            return(
-            <li key = {todo}>
-            <div className="list-row">
-              <p>{todo}</p>
-              <button onClick={() => onClickComplete(index)}>完了</button>
-              <button onClick={() => onClickDelete(index)}>削除</button>
-            </div>
-            </li>);
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
+                </div>
+              </li>
+            );
           })}
         </ul>
       </div>
       <div className="complete-area">
-      <p className="title">完了のTODO</p>
+        <p className="title">完了のTODO</p>
         <ul>
-        {completeTodos.map((todo, index) => {
-            return(
-            <li key = {todo}>
-            <div className="list-row">
-              <p>{todo}</p>
-              <button onClick={() => onClickBack(index)}>戻す</button>
-            </div>
-            </li>);
+          {completeTodos.map((todo, index) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
+                </div>
+              </li>
+            );
           })}
         </ul>
       </div>
     </>
-  )
+  );
 }
